7c6b2a3483b93c75d0cd222f077cd45b2b9402c9
yeobdoll <jsysu2846@naver.com>
2021-06-06 17:38:51 +0900

    [14'] rebase 성공

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 # Hello World
-
 print("hello world")
 
 a = 10
 b = 20
 
 # Calc
-c = a + b
-print(c)
+d = b + c
 
-# conflict
-print("conflict")
+
+
+print(d)
+print(a + b)
+print(a * b)
+# 대화
+name = "yeob"
+print("안녕! 난 {}이라고해 잘부탁해!".format(name))
+q = input("넌 어디서 왔니? ")
+print(q)
